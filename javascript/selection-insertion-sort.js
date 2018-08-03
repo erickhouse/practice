29f77a78791bbe5720e53a162968497e0403a8d6
erickhouse <erickhouse01@gmail.com>
2018-08-03 09:21:51 -0700

    Update and rename selection-sort.js to selection-insertion-sort.js

--- javascript/selection-insertion-sort.js
+++ javascript/selection-insertion-sort.js
@@ -1,4 +1,5 @@
 
+
 /**
  * 
  * @param {number[]} arr 
@@ -7,17 +8,31 @@
  * An undefined compared against an numeric value always returns false.
  */
 function selectionSort(arr) {
-
     for(let i = 0; i < arr.length; i++) {
         let min = i;
-
         for(let j = i + 1; j < arr.length; j++) {
             if(arr[j] < arr[min]) {
                 min = j;
             }
         }
-
         [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+    return arr;
+}
+
+/**
+ * 
+ * @param {number[]} arr 
+ * @description on a completely sorted array the runtime is only O(n) given that is only has to inspect the element to its left.
+ * Selction sort always has to scan the rest of the array to find the absolute minium.
+ */
+function insertionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i; j >= 0; j--) {            
+            if(arr[j] < arr[j-1]) {
+                [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+            }
+        }
     }
     return arr;
 }
