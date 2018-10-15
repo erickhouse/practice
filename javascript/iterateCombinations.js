cc72110e0e94503b0819331111e844a2c1099a82
erickhouse <erickhouse01@gmail.com>
2018-10-14 18:21:13 -0700

    Update iterateCombinations.js

--- javascript/iterateCombinations.js
+++ javascript/iterateCombinations.js
@@ -1,7 +1,7 @@
 
 /**
  * Creates all combinations of all numbers in the Set
- * It doesn this this through iterations instead of the traditional 
+ * It does this this through iterations instead of the traditional 
  * recursive method
  * @param {Set<number>} arr 
  * @returns {Set<Set<number>>}
